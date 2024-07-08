59995ce22b07f0270daf99b9183e83b31caf9ed3
Nombulelo <lelonkalanp@gmail.com>
2024-07-08 16:36:58 +0200

    Version1
    Made changes by removing the about to its single page
    Created a footer and modified the navigation bar

--- nombulelo/web.py
+++ nombulelo/web.py
@@ -8,6 +8,9 @@ web = Flask(__name__)
 def home():
     return render_template("MyProfile.html")
 
+@web.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     web.run(debug=True)
