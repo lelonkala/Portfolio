53c3578790bc46c4b2dfa96a67838609827fe9ae
Nombulelo <lelonkalanp@gmail.com>
2024-07-18 11:31:52 +0200

    modified the About page
    Fun facts section

---
+++ nombulelo/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    message = db.Column(db.Text, nullable=False)
--- nombulelo/web.py
+++ nombulelo/web.py
@@ -1,16 +1,56 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Contact
 
 web = Flask(__name__)
 
-#home page
+# Database configuration
+web.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+web.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(web)
+with web.app_context():
+    db.create_all()
+
+#home page route
 @web.route('/')
 def home():
     return render_template("MyProfile.html")
 
+@web.route('/contact')
+def contact():
+    return render_template("form.html")
+
+
+
+@web.route('/submit_contact_form', methods=['POST'])
+def submit_contact_form():
+    
+    new_contact = Contact(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        phone=phone,
+        message=message
+    )
+    
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    phone = request.form['phone']
+    message = request.form['message']
+    
+    # Process the form data
+    db.session.add(new_contact)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 @web.route('/about')
 def about():
     return render_template('about.html')
+
+@web.route('/projects')
+def project():
+    return render_template('projects.html')
 
 if __name__ == '__main__':
     web.run(debug=True)
